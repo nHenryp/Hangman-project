c66ab296f28e7439c4a4d648ea2d3ba2d31d33b0
root <root@DESKTOP-4A6NNJ6.>
2024-06-24 18:32:55 +0100

    first day

---
+++ js/app.js
@@ -0,0 +1,159 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*as a user i want to be able to see an updated score as i play along
+as a user i want to see visual actions for wrong choices. i.e 
+  the parts of Hangman.
+  disapearring letters.
+ as a user i want to be able to mute the music on the page.
+ as a user correct letter choices should be displayed on the screen.
+ i want the game to randomize the word choices.
+ a clear message sould be displayed when winning a round of the game
+ a reset button should be available 
+ and hint button should be present if user needs assistance.
+
+
+*/
+
+
+
+ /*-------------------------------- Constants --------------------------------*/
+
+/*
+let gameBoard ;
+
+const correctWord; make array
+
+
+/*-------------------------------- Variables --------------------------------*/
+/*
+let wordChoice = ['orange', 'apple']
+let selectedWord = math.floor...
+let selectedLetter = [] to save letters
+ 
+let win = false;
+let scoreBoard = 0;
+
+let maxGuesses = 5
+
+
+
+
+*/
+/*------------------------ Cached Element References ------------------------*/
+
+/*
+const selectedWords
+const randomWord
+const buttonElement =
+const guessedLetters =
+const scoreBoard=
+
+const keyPadBtn =
+const restartbtn
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+/*
+const randomWord = () => {
+math.floor and math.random to get word choice from array
+
+
+}
+
+const getPlayerGuess () {
+get player letter choices
+
+if statments
+
+
+
+}
+
+
+
+
+
+const checkWin = () => {
+see if players choices = random word
+}
+   
+const checkLoss = () => {
+check if maxguesses have been used  
+}
+
+
+const updateGame() =>
+update players score, + points for correct guesses
+
+
+
+
+
+
+const checkLetter = () =>{
+   if guess === randomWord move to next round
+}
+
+const displayResult = () => {
+display win or lose on the screen
+}
+ 
+
+const scoreBoard = () => {
+
+ }
+
+
+const hintBtn = () => {
+
+}
+
+
+
+const init =() => {
+
+}
+
+const resetGame = () => {
+    } 
+
+   const endGame = () => {
+    
+    }
+
+
+render()
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+ /*
+buttonElement.forEach(keyPadBtn => {
+  keyPadBtn.addEventListener('click','resetGame')*/
+  
+
+  //create Array
+
+  //diplay buttons
+  //check if word chossen includes a letter that wwas picked
+  //if it happens i want something to happen
+  //if not something else.
+  
